f9d8b3bc8efab0782a58ed1191e0d3fb67c276ca
lLeonerde <leonardo.rborbon@gmail.com>
2022-11-10 17:28:38 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia(numero:"129999999", modelo:"lumia",imei:"1231313",memoria:64);
+
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Clash of clans");
+Iphone iphone = new Iphone(numero:"999999", modelo:"S5", imei:"12938123",memoria:32);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("zap zap");
